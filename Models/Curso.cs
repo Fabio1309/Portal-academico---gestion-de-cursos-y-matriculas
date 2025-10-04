ee49dd6b33ff3b475c17ead5bfe368fee002eced
Fabio Enrique Saavedra <saavedragarciafabio@gmail.com>
2025-10-04 03:05:03 -0500

    creacion de views

---
+++ Models/Curso.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace examen_parcial_programacion1.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Codigo { get; set; } = string.Empty;
+        
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+        
+        public string? Descripcion { get; set; }
+        
+        public int Creditos { get; set; }
+        
+        public TimeOnly HorarioInicio { get; set; }
+        
+        public TimeOnly HorarioFin { get; set; }
+        
+        // Relación con Matriculas
+        public ICollection<Matricula> Matriculas { get; set; } = new List<Matricula>();
+    }
+}
---
+++ Models/Matricula.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace examen_parcial_programacion1.Models
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+        
+        // Relación con Curso
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; } = null!;
+        
+        // Relación con Usuario (IdentityUser)
+        public string UsuarioId { get; set; } = string.Empty;
+        public IdentityUser Usuario { get; set; } = null!;
+        
+        public DateTime FechaMatricula { get; set; } = DateTime.Now;
+        
+        public bool Activa { get; set; } = true;
+    }
+}
